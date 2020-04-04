d8ef42b4f89048c38c727c63662dafed967c3224
juancastillo0 <juancastillo0@users.noreply.github.com>
2020-04-04 18:30:43 +0000

    deploy: 1f36ebcda01a79b00eb7e147c121c4da81d569cf

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,13 +4,14 @@ const RESOURCES = {
   "/manifest.json": "e84956a5f9ed0f157efb099a928923eb",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/assets/AssetManifest.json": "38c3f84913b58fc1d0c52fb69eb60cd9",
+"/assets/AssetManifest.json": "eca79b7671996c553eaee20b56baeeaa",
 "/assets/LICENSE": "c834fbc123c09d8f5e5f8825090ca8a1",
+"/assets/assets/parking-hypermarket.jpg": "4849f52f776d7b3853a862ff5887b031",
 "/assets/assets/place-image.png": "c33812e888708344785de30b2f28fd96",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/main.dart.js": "5ea534eb4ab77e0018a3349c43346ea2",
+"/main.dart.js": "a34d8bc6d011e9b262700e125972f823",
 "/index.html": "ccecc3c32da4797b0a2a50fd66b9d6bf"
 };
 
