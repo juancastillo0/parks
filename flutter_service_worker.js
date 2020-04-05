8a7b84dcc168eec351add3ada3fc70dec74c320b
juancastillo0 <juancastillo0@users.noreply.github.com>
2020-04-05 21:15:41 +0000

    deploy: f7665d94dca623520c6a41a468c22b87eb4edc28

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,7 +11,7 @@ const RESOURCES = {
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/main.dart.js": "a34d8bc6d011e9b262700e125972f823",
+"/main.dart.js": "ad180d82dcb430cb6507ef1bee62555f",
 "/index.html": "ccecc3c32da4797b0a2a50fd66b9d6bf"
 };
 
