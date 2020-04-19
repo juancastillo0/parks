2e4849e4a6316d40d602e31ef4c4fd510f60e122
juancastillo0 <juancastillo0@users.noreply.github.com>
2020-04-19 04:32:15 +0000

    deploy: 3719d4a59c214c0b67c61807682094e36911170f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,18 +1,19 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/manifest.json": "e84956a5f9ed0f157efb099a928923eb",
-"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/assets/AssetManifest.json": "eca79b7671996c553eaee20b56baeeaa",
-"/assets/LICENSE": "c834fbc123c09d8f5e5f8825090ca8a1",
-"/assets/assets/parking-hypermarket.jpg": "4849f52f776d7b3853a862ff5887b031",
-"/assets/assets/place-image.png": "c33812e888708344785de30b2f28fd96",
-"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/main.dart.js": "ad180d82dcb430cb6507ef1bee62555f",
-"/index.html": "ccecc3c32da4797b0a2a50fd66b9d6bf"
+  "index.html": "ccecc3c32da4797b0a2a50fd66b9d6bf",
+"/": "ccecc3c32da4797b0a2a50fd66b9d6bf",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/assets/parking-hypermarket.jpg": "4849f52f776d7b3853a862ff5887b031",
+"assets/assets/place-image.png": "c33812e888708344785de30b2f28fd96",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/AssetManifest.json": "eca79b7671996c553eaee20b56baeeaa",
+"assets/LICENSE": "90174f4fe4bb4eaa093b2d4cf7467ab6",
+"main.dart.js": "590d34b4254e1855ab12505a0075c83a",
+"manifest.json": "e84956a5f9ed0f157efb099a928923eb"
 };
 
 self.addEventListener('activate', function (event) {
